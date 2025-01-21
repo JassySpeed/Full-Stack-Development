b3b47a411bd74ba7139b73f3eac2aeebb4ed84b1
JassySpeed <122783656+JassySpeed@users.noreply.github.com>
2025-01-21 19:05:29 +0530

    Introduction to JavaScript

---
+++ Day019/first.js
@@ -0,0 +1,26 @@
+console.log("Hello Coder Army");
+
+// variable declarations
+// let const var
+// var is old method, dont't use it 
+
+
+// let
+// can't re-declare your varibale
+let num=10;
+console.log(num);
+
+let string ="jasmeet";
+console.log(string);    
+
+// const
+// can't reassign your varibale.
+const id=20;
+console.log(id);
+
+
+// var
+
+var n=50;
+var n=60;
+console.log(n);
