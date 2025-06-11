03c2fdc9889ffbaab729ebbdeffc5b582495d1df
JassySpeed <122783656+JassySpeed@users.noreply.github.com>
2025-06-12 00:40:26 +0530

    JavaScript

---
+++ Day020/first.js
@@ -0,0 +1,55 @@
+// number
+
+let x=10;
+console.log(x);
+
+let y=10.101;
+console.log(typeof y);
+
+console.log(Number.MAX_SAFE_INTEGER);
+console.log(Number.MIN_SAFE_INTEGER);
+
+
+// string
+
+let ans="jasmeet"
+console.log(ans);
+
+let nam='rohit negi sir ji';
+console.log(nam);
+
+// boolean
+
+// true or false
+let bool=true;
+console.log(bool);
+
+// undefined
+
+// un assigned value
+let z;
+console.log(z);
+
+
+// null
+
+// The special null value does not belong to any of the default data types. It forms a separate type of its own which contains only the null value
+// when you intentionally put nothing inside the variable, so then you have to put null inside it.
+// null is object.
+let q=null;
+console.log(q);
+console.log(typeof q);
+
+
+
+// BigInt
+
+let w = 1234658912345678913456879n;
+console.log(w);
+
+
+
+
+// Symbol
+// we will talk about this later
+
