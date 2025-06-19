99e0b2b050404fa10929c4461cbcefea029613a0
JassySpeed <122783656+JassySpeed@users.noreply.github.com>
2025-06-19 15:47:42 +0530

    Math in JS

---
+++ Day024/math.js
@@ -0,0 +1,53 @@
+// let num1=123;
+// let num2=new Number(123);
+// let num3=new Number(123);
+// console.log(num1==num2);
+// console.log(num2==num3);
+
+///////////////////////////////////////////////////////
+// toFixed meaning how many number you want after the decimal. number get round-off.
+// toPrecision meaning how many number you want in total.
+// toExponential meaning after the deciaml how many number you want in exponential.
+// let n=123.52;
+// console.log(n.toFixed(1));
+// console.log(n.toPrecision(3));
+// console.log(n.toExponential(4));
+// console.log(n.toString()); // convert into string.
+// console.log(n.valueOf()); // just return the value.
+
+////////////////////////////////////////////////////
+// Math
+// console.log(Math.E);
+// console.log(Math.PI);
+// console.log(Math.LN10);
+// console.log(Math.LOG10E);
+
+///////////////////////////////////////////////////
+// floor and ceil 
+// let num=23.9;
+// let num1=123.1;
+// console.log(Math.floor(num));
+// console.log(Math.ceil(num1));
+
+// random will generate random value.
+// 0<=value<1
+// console.log(Math.floor(Math.random()*10));
+
+// for 1-10
+// console.log(Math.floor(Math.random()*10)+1);
+
+// 11-20
+// console.log(Math.floor(Math.random()*10)+11);
+
+// For Ludo auto generation of 1 to 6 value.
+// console.log(Math.ceil(Math.random()*6));
+
+
+// Formula for generating any value from x to y.
+// console.log(Math.floor(Math.random()*(max-min+1)+min));
+
+// for 30-40
+// console.log(Math.floor(Math.random()*(40-30+1)+30));
+
+// for ludo 1-6
+// console.log(Math.floor(Math.random()*(6-1+1)+1));
